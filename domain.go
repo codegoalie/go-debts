ef62ad460020b838dae28d3b37919a20bd277614
Chris Marshall <chris@chrismar035.com>
2014-12-25 23:27:01 -0500

    Can MakePayment

---
+++ domain.go
@@ -0,0 +1,29 @@
+package domain
+
+import (
+	"time"
+)
+
+type Debitor struct {
+	Id int
+	Name string
+	Accounts []Account
+}
+
+type Account struct {
+	Id int
+	Name string
+	Payments []Payment
+}
+
+type Payment struct {
+	Id int
+	Amount float64
+	Balance float64
+	Date time.Time
+}
+
+func (account *Account) MakePayment(payment Payment) error {
+	account.Payments = append(account.Payments, payment)
+	return nil
+}
---
+++ domain_test.go
@@ -0,0 +1,16 @@
+package domain
+
+import (
+	"testing"
+	"time"
+)
+
+func TestMakePayment(t *testing.T) {
+	account := Account{1, "Debt Account Name", []Payment{{2, 300, 3000, time.Now()}}}
+	newPayment := Payment{1, 200, 2000, time.Now()}
+	account.MakePayment(newPayment)
+	lastPayment := account.Payments[len(account.Payments)-1]
+	if (lastPayment != newPayment) {
+		t.Errorf("MakePayment adds payment. Expected: %f; Got: %f", newPayment.Amount, lastPayment.Amount)
+	}
+}
