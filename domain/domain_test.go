f4ede3563cd00e486c679d15e9264f10c988822b
Chris Marshall <chris@chrismar035.com>
2015-01-05 17:47:10 -0500

    Ensure accounts can find their last payment

--- domain/domain_test.go
+++ domain/domain_test.go
@@ -24,3 +24,42 @@ func TestAddPayment(t *testing.T) {
 		t.Errorf("Add adds payment. Expected: %f; Got: %f", newPayment.Amount, lastPayment.Amount)
 	}
 }
+
+func TestLastPaymentNoPayments(t *testing.T) {
+	account := Account{1, "Debt Account Name", []Payment{}}
+	_, err := account.LastPayment()
+	if err == nil {
+		t.Errorf("LastPayment with no payments. Expected error, but got none.")
+	}
+}
+
+func TestLastPaymentOnePayment(t *testing.T) {
+	payment := Payment{2, 300, 3000, time.Now()}
+	account := Account{1, "Debt Account Name", []Payment{payment}}
+	lastPayment, _ := account.LastPayment()
+	if lastPayment != payment {
+		t.Errorf("LastPayment with one payment. Expected: %f; Got: %f", payment.Amount, lastPayment.Amount)
+	}
+}
+
+func TestLastPaymentTwoOrderedPayment(t *testing.T) {
+	minusOneDay, _ := time.ParseDuration("-24h")
+	firstPayment := Payment{1, 300, 3000, time.Now().Add(minusOneDay)}
+	secondPayment := Payment{2, 320, 2700, time.Now()}
+	account := Account{1, "Debt Account Name", []Payment{firstPayment, secondPayment}}
+	lastPayment, _ := account.LastPayment()
+	if lastPayment != secondPayment {
+		t.Errorf("LastPayment with two ordered payments. Expected: %f; Got: %f", secondPayment.Amount, lastPayment.Amount)
+	}
+}
+
+func TestLastPaymentTwoUnorderedPayment(t *testing.T) {
+	minusOneDay, _ := time.ParseDuration("-24h")
+	firstPayment := Payment{1, 320, 2700, time.Now()}
+	secondPayment := Payment{2, 300, 3000, time.Now().Add(minusOneDay)}
+	account := Account{1, "Debt Account Name", []Payment{firstPayment, secondPayment}}
+	lastPayment, _ := account.LastPayment()
+	if lastPayment != firstPayment {
+		t.Errorf("LastPayment with two unordered payments. Expected: %f; Got: %f", firstPayment.Amount, lastPayment.Amount)
+	}
+}
