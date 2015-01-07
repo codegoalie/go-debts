7bf94490e8cdfdc08b690200f58adb74835a2773
Chris Marshall <chris@chrismar035.com>
2015-01-07 18:33:16 -0500

    Implement Sqlite database handler

---
+++ infrastructure/sqlitehandler.go
@@ -0,0 +1,46 @@
+package infrastructure
+
+import (
+	"database/sql"
+	"go-debts/interfaces"
+	"fmt"
+)
+
+type SqliteHandler struct {
+	Conn *sql.DB
+}
+
+type SqliteRow struct {
+	Rows *sql.Rows
+}
+
+func NewSqliteHandler(dbPath string) interfaces.DbHandler {
+	conn, _ := sql.Open("sqlite3", dbPath)
+	sqliteHandler := new(SqliteHandler)
+	sqliteHandler.Conn = conn
+	return sqliteHandler
+}
+
+func (handler *SqliteHandler) Execute(statement string) {
+	handler.Conn.Exec(statement)
+}
+
+func (handler *SqliteHandler) Query(statement string) interfaces.Row {
+	rows, err := handler.Conn.Query(statement)
+	if err != nil {
+		fmt.Println(err)
+		return new(SqliteRow)
+	}
+
+	row := new(SqliteRow)
+	row.Rows = rows
+	return row
+}
+
+func (row *SqliteRow) Scan(dest ...interface{})  {
+	row.Rows.Scan(dest...)
+}
+
+func (row *SqliteRow) Next() bool {
+	return row.Rows.Next()
+}
